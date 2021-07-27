1ba3e8af8d056e6c909b0fb2c05c8d4333525b4a
kerem <kerem@phonoclick.com>
2021-07-27 20:53:25 +0300

    order api configure automapper

---
+++ Services/Order/FreeCource.API.Order.Application/Dtos/CreatedOrderDto.cs
@@ -0,0 +1,7 @@
+﻿namespace FreeCource.API.Order.Application.Dtos
+{
+  public class CreatedOrderDto
+  {
+    public int Id { get; set; }
+  }
+}
---
+++ Services/Order/FreeCource.API.Order.Application/Mapping/CustomMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FreeCource.API.Order.Application.Dtos;
+using FreeCource.API.Order.Domain.OrderAggregate;
+
+namespace FreeCource.API.Order.Application.Mapping
+{
+  internal class CustomMapping : Profile
+  {
+    public CustomMapping()
+    {
+      CreateMap<Domain.OrderAggregate.Order, OrderDto>().ReverseMap();
+      CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+      CreateMap<Address, AddressDto>().ReverseMap();
+    }
+  }
+}
