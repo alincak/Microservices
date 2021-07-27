fb1a75d1c4f9844cbca18e6f10ffa0339d0afd04
kerem <kerem@phonoclick.com>
2021-07-27 20:26:46 +0300

    order aggregate

---
+++ Services/Order/FreeCource.API.Order.Domain/OrderAggregate/OrderItem.cs
@@ -0,0 +1,28 @@
+﻿using FreeCource.API.Order.Domain.Core;
+
+namespace FreeCource.API.Order.Domain.OrderAggregate
+{
+  public class OrderItem : Entity
+  {
+    public string ProductId { get; private set; }
+    public string ProductName { get; private set; }
+    public string PictureUrl { get; private set; }
+    public decimal Price { get; private set; }
+
+    public OrderItem(string productId, string productName, string pictureUrl, decimal price)
+    {
+      ProductId = productId;
+      ProductName = productName;
+      PictureUrl = pictureUrl;
+      Price = price;
+    }
+
+    public void Update(string productName, string pictureUrl, decimal price)
+    {
+      ProductName = productName;
+      PictureUrl = pictureUrl;
+      Price = price;
+    }
+
+  }
+}
