9f724ceb22be0588065d2e06211bfde9db496b50
kerem <kerem@phonoclick.com>
2021-08-07 19:27:14 +0300

    created api gateway with Ocelot

--- Services/Catalog/FreeCourse.API.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.API.Catalog/Program.cs
@@ -1,5 +1,8 @@
+using FreeCourse.API.Catalog.Services;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Linq;
 
 namespace FreeCourse.API.Catalog
 {
@@ -7,7 +10,22 @@ namespace FreeCourse.API.Catalog
   {
     public static void Main(string[] args)
     {
-      CreateHostBuilder(args).Build().Run();
+      var host = CreateHostBuilder(args).Build();
+
+      using (var scope = host.Services.CreateScope())
+      {
+        var serviceProvider = scope.ServiceProvider;
+
+        var categoryService = serviceProvider.GetRequiredService<ICategoryService>();
+
+        if (!categoryService.GetAllAsync().Result.Data.Any())
+        {
+          categoryService.CreateAsync(new Dtos.CategoryDto { Name = "Asp.Net Core Kursu" }).Wait();
+          categoryService.CreateAsync(new Dtos.CategoryDto { Name = "Asp.Net Core API Kursu" }).Wait();
+        }
+      }
+
+      host.Run();
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
