4b466dc922674606621c54837e7d8bf8ba4b4ac3
kerem <kerem@phonoclick.com>
2021-08-08 13:43:28 +0300

    configurations appsettings

---
+++ Clients/FreeCource.Web/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,14 @@
+﻿using FreeCource.Web.Models;
+using FreeCourse.Shared.Dtos;
+using IdentityModel.Client;
+using System.Threading.Tasks;
+
+namespace FreeCource.Web.Services.Interfaces
+{
+  interface IIdentityService
+  {
+    Task<Response<bool>> SignIn(SigninInput signinInput);
+    Task<TokenResponse> GetAccessTokenByRefreshToken();
+    Task RevokeRefreshToken();
+  }
+}
