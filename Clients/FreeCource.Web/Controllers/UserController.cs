84cbd0560847751f1cc1c527a64d501599fb1d17
kerem <kerem@phonoclick.com>
2021-08-10 22:32:08 +0300

    user index view

---
+++ Clients/FreeCource.Web/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using FreeCource.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FreeCource.Web.Controllers
+{
+  [Authorize]
+  public class UserController : Controller
+  {
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+      _userService = userService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+      return View(await _userService.GetUser());
+    }
+  }
+}
--- Clients/FreeCource.Web/Startup.cs
+++ Clients/FreeCource.Web/Startup.cs
@@ -24,6 +24,8 @@ namespace FreeCource.Web
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddScoped<ResourceOwnerPasswordTokenHandler>();
+
       services.AddHttpContextAccessor();
       services.AddHttpClient<IIdentityService, IdentityService>();
 
