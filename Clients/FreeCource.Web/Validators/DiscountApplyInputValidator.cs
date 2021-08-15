1e78705ea85c441f4243a24ab48b8685135df0c3
kerem <kerem@phonoclick.com>
2021-08-15 15:25:43 +0300

    discount model and validator

---
+++ Clients/FreeCource.Web/Validators/DiscountApplyInputValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using FreeCource.Web.Models.Discounts;
+
+namespace FreeCource.Web.Validators
+{
+  public class DiscountApplyInputValidator : AbstractValidator<DiscountApplyInput>
+  {
+    public DiscountApplyInputValidator()
+    {
+      RuleFor(x => x.Code).NotEmpty().WithMessage("indirim kupon alanı boş olamaz");
+    }
+  }
+}
