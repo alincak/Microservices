51cfeca505f1c4181b30714d189622bc7ed376fa
kerem <kerem@phonoclick.com>
2021-08-15 16:32:22 +0300

    order service

--- Clients/FreeCource.Web/Extensions/ServiceExtension.cs
+++ Clients/FreeCource.Web/Extensions/ServiceExtension.cs
@@ -45,6 +45,12 @@ namespace FreeCource.Web.Extensions
       {
         opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.Payment.Path}");
       }).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
+
+      services.AddHttpClient<IOrderService, OrderService>(opt =>
+      {
+        opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.Order.Path}");
+      }).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
+
     }
   }
 }
